88feb808e059c7f49730fb079a826a5982aea478
jonathanameri <jonathan.ameri@uconn.edu>
2023-02-09 14:56:59 -0500

    add PA week 4

---
+++ PA/PA-week4-startercode/ECMergeIntervals.cpp
@@ -0,0 +1,12 @@
+// Merge overlapping intervals and return an array of the non-overlapping intervals that cover all the intervals in the input
+// Your code must run no slower than O(nlogn) where n is the number of intervals
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void ECMergeIntervals(const std::vector<std::pair<int,int> > &listIntervals, std::vector<std::pair<int,int> > &listNonOverlapIntervals)
+{
+  // your code here...
+}
